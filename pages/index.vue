b3445b88b6618e6e35c307729cd5e4ecd8fc286a
Kanu Chukwunonso <franknonso114@gmail.com>
2021-12-21 22:08:34 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="font-body container">
+    <h1 class="text-5xl">Hi, I'm Chukwunonso</h1>
+    <p>FullStack JavaScript Engineer based in Amsterdam</p>
+  </div>
+</template>
+
+<script>
+
+</script>

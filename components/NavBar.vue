cd6c90c09464cf05b3b40b557697fcc58710b87d
Kanu Chukwunonso <franknonso114@gmail.com>
2021-12-22 00:04:03 +0100

    Build out the NavBar component UI

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="grid grid-cols-2 mt-5">
+      <div>
+          <img src="~/assets/img/profile.jpg" alt="profile-picture">
+      </div>
+      <div class="self-center justify-self-end">
+          <button class="font-body font-normal py-2 px-6 text-sm rounded-[12px] bg-black text-white subpixel-antialiased">Menu</button>
+      </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return ''
+    }
+}
+</script>
+
+
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="font-body container">
-    <h1 class="text-5xl font-bold">Hi, I'm Chukwunonso</h1>
-    <p>FullStack JavaScript Engineer based in Amsterdam</p>
+  <div class="container mx-auto px-6">
+    <NavBar />
   </div>
 </template>
 

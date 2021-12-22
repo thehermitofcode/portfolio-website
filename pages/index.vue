0cc772ec323f0d9235b9947420d40298c59cf9a6
Kanu Chukwunonso <franknonso114@gmail.com>
2021-12-22 15:24:54 +0100

    chore: Fix paddings at breakpoints

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container mx-auto px-6">
+  <div class="container mx-auto px-6 lg:px-32">
     <NavBar />
-    <section class="mt-16">
+    <section class="mt-16 max-w-2xl">
       <p class="font-body font-normal subpixel-antialiased text-[16px] leading-6">Hey, I’m Chukwunonso, a full-stack JavaScript engineer, open-source tinkerer and technical content writer currently building things with Vue and Node.</p>
       <p class="font-body font-normal subpixel-antialiased text-[16px] leading-6 mt-12">I share knowledge on how to write software better through articles on my blog.</p>
       <p class="font-body font-normal subpixel-antialiased text-[16px] leading-6 mt-12">You can find me on Twitter where I talk about all things design, code and software.</p>
